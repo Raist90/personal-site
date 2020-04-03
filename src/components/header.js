aec2ca6402359b21f88ef8a7720cb535ffcdde50
Raist90 <manuel.detoma@gmail.com>
2020-04-03 18:54:56 +0200

    added it language to html

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet'
 const Header = () => {
     return (
         <Helmet>     
+            <html lang="it"></html>
             <link href="https://fonts.googleapis.com/css?family=Arvo|Cabin|Indie+Flower&display=swap" rel="stylesheet"></link>
         </Helmet>
     )
