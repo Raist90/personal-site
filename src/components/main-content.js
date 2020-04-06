226562c5bd8a21942da5309c7392832600017d5d
Raist90 <manuel.detoma@gmail.com>
2020-04-06 16:32:19 +0200

    transformer remark plugin added

--- src/components/main-content.js
+++ src/components/main-content.js
@@ -23,7 +23,7 @@ const MainContent = () => {
                         <div class="m-featured-img">
                             <img src={ BadalonaBeach } class="img-fluid" alt="badalona beach" />
                         </div>
-                        <h1>Titolo articolo</h1>
+                        <h2>Titolo articolo</h2>
                         <p class="subtitle yellow-text">Sottotitolo</p>
                         <p>
                             Nulla amet do nulla et amet reprehenderit. Do et occaecat nisi anim eiusmod excepteur aliqua minim. Sit aliqua nulla sunt dolor duis fugiat occaecat culpa eiusmod duis ut aute ullamco. Sit ut duis eiusmod commodo. Deserunt Lorem occaecat enim deserunt tempor nulla proident enim sunt.
